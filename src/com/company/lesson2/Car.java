f18555689c29146f1d7fc80d0ff3dbd5e70c4d22
Alex Hazniuk <ohazniuk@gmail.com>
2023-03-04 01:10:41 +0200

    implemented task 'enter oop'

---
+++ src/com/company/lesson2/Car.java
@@ -0,0 +1,21 @@
+package com.company.lesson2;
+
+public class Car {
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+        System.out.println("Electricity started");
+    }
+
+    private void startCommand() {
+        System.out.println("Command started");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("Fuel system started");
+    }
+}
---
+++ src/com/company/lesson2/repeated_name/SameName.java
@@ -0,0 +1,4 @@
+package com.company.lesson2.repeated_name;
+
+public class SameName {
+}
