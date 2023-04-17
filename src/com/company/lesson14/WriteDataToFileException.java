b685c381efe21ea7335b0d77465d21040d9b40d3
Alex Hazniuk <ohazniuk@gmail.com>
2023-04-17 20:50:44 +0300

    implemented task 'Parser'

---
+++ src/com/company/lesson14/WriteDataToFileException.java
@@ -0,0 +1,7 @@
+package com.company.lesson14;
+
+public class WriteDataToFileException extends RuntimeException {
+    public WriteDataToFileException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
